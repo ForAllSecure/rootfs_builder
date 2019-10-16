853bc2897172e30ae6b9f984ca38541ce2721204
marlies ruck <marlies@forallsecure.com>
2019-10-16 14:09:19 -0700

    adding more tests

--- test/integration.sh
+++ test/integration.sh
@@ -2,14 +2,15 @@
 #
 # Pull apline:3.10, extract the rootfs, and verify its hash
 
-set -xe
+set -e
 
 # set up
+printf "testing extracting alpine 3.10...\n"
 test_dir="/test/"
 rm -rf $test_dir
 
 # run
-go run main.go test/config.json
+go run main.go test/alpine.json
 
 # check config hash
 config_md5=`md5sum $test_dir/config.json | head -n1 | awk '{print $1;}'`
@@ -25,6 +26,42 @@ correct_rootfs_md5="483a1dff1f39a232b3a876a99f9f8cd4"
 echo $rootfs_md5
 if [ "$rootfs_md5" != "$correct_rootfs_md5" ]; then
     echo "rootfs doesn't match"
+    exit 1
+fi
+
+# tear down
+rm -rf $test_dir
+
+# set up
+printf "testing chowning to subuid...\n"
+test_dir="/test/"
+rm -rf $test_dir
+
+# run
+go run main.go test/subuid.json
+
+# Check that we chowned to the subuid mapping 100000
+uid=`ls -ld /test/rootfs/bin/cat  | awk '{print $3}'`
+if [ "$uid" != "100000" ]; then
+    echo "failed to chown to subuid 100000"
+    exit 1
+fi
+
+# tear down
+rm -rf $test_dir
+
+# set up
+printf "testing chowning to a user besides root...\n"
+test_dir="/test/"
+rm -rf $test_dir
+
+# run
+go run main.go test/fas.json
+
+# Check that we chowned to the subuid mapping 100000
+uid=`ls -ld /test/rootfs/bin/cat  | awk '{print $3}'`
+if [ "$uid" != "fas" ]; then
+    echo "failed to chown to fas"
     exit 1
 fi
 
